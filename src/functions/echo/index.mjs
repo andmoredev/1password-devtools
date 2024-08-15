c19fa9f87bea078738e160c5169790571c8cc402
Andres Moreno <andrez.moreno.m@gmail.com>
2024-08-15 10:20:13 -0600

    use correct repo as a base

--- src/functions/echo/index.mjs
+++ src/functions/echo/index.mjs
@@ -1,14 +1,10 @@
 import { initializePowertools, logger } from '../shared/lambda-powertools.mjs';
 import { getResponse } from '../shared/apigateway.mjs';
-import schema from './submitted-incident.json' with { type: 'json' };
 
 export const handler = initializePowertools(async (event) => {
   try {
     const input = JSON.parse(event.body);
 
-    const apiKey = 'op://Dev Secrets/SendgrID/Section_lx46ortdghtmeb5it57xss5dce/api key';
-    console.log('apiKey', apiKey);
-    console.log('schema', schema);
     return getResponse(200, input);
   } catch (err) {
     logger.error(err, err.stack);
--- src/functions/echo/tests/index.tests.mjs
+++ src/functions/echo/tests/index.tests.mjs
@@ -1,11 +1,6 @@
 
 import * as app from '../index.mjs';
 import { expect } from 'chai';
-import { $RefParser } from '@apidevtools/json-schema-ref-parser';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import fs from 'fs-extra';
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 describe('Echo', () => {
   describe('handler', () => {
@@ -28,20 +23,4 @@ describe('Echo', () => {
       expect(body).to.have.property('message', 'Something went wrong!');
     });
   });
-
-  before(async () => {
-    const baseSchema = await import('../../shared/schemas/submitted-incident.json', {
-      with: { type: 'json' }
-    });
-    const dereferencedSchema = await $RefParser.dereference(`${__dirname}/../../shared/schemas/`, baseSchema, {
-      mutateInputSchema: false,
-      dereference: {
-        circular: 'ignore'
-      }
-    });
-
-    console.log(JSON.stringify(dereferencedSchema.default));
-
-    fs.writeFileSync(`${__dirname}/../submitted-incident.json`, JSON.stringify(dereferencedSchema.default, null, 2));
-  })
 });
