b3fa114590fe44415bc05c90ef95d6e643136bdb
Andres Moreno <andrez.moreno.m@gmail.com>
2024-08-15 16:51:13 -0600

    try again

--- src/functions/echo/index.mjs
+++ src/functions/echo/index.mjs
@@ -5,7 +5,6 @@ export const handler = initializePowertools(async (event) => {
   try {
     const input = JSON.parse(event.body);
 
-    // I want to push something to check for signed commit
     return getResponse(200, input);
   } catch (err) {
     logger.error(err, err.stack);
